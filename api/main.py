5a12875c7cbab85528bd6361ad259d7960a15bd4
Rendon(PC WINDOWS) <jjrendongg@gmail.com>
2026-07-10 07:08:55 +0000

    test: CI/CD

--- api/main.py
+++ api/main.py
@@ -25,7 +25,7 @@ async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
-app = FastAPI(title="API Backend - CI/CD", lifespan=lifespan)
+app = FastAPI(title="API Backend - CI/CD :D", lifespan=lifespan)
 
 def get_session():
     with Session(engine) as session:
